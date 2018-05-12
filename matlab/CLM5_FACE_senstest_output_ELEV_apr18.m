87b5d7cbfcc931d7c6693c453c9add1e9625489d
rosiealice <rfisher@cheyenne4.ib0.cheyenne.ucar.edu>
2018-05-11 18:05:45 -0600

    ELEV matlab script changes

--- matlab/CLM5_FACE_senstest_output_ELEV_apr18.m
+++ matlab/CLM5_FACE_senstest_output_ELEV_apr18.m
@@ -14,30 +14,28 @@ mkfigs=1
 end
 figdir = 'figures'
 u=3600*24*365'
-vars         = ({'GPP','NPP','TLAI','QVEGT','TOTVEGC','BTRANMN','COST_NFIX','COST_NRETRANS','NPP_NUPTAKE','NACTIVE','NFIX','NRETRANS',})
-namevars     = ({'Gross Production (gCm^{-2}y^{-1})','Net Production (gCm^{-2}y^{-1})','Leaf Area Index (m^{2}m^{-2})','QVEGT','Total Veg Carbon (gm^{-2})' ,'BTRANMN' ,'COST NFIX','COST RETRANS' ,'N uptake cost (gCm^{-2}y^{-1})','NACTIVE','NFIX','NRETRANS'})
-%maxy            = [3050  1400    6    1500    15700             1       10           10           0.4              10         10         5]
-maxy            = [2350  1100    7    1500    19000             1       10           10              0.4           10         10       5]
-unit_conversion = [u      u      1      u      1             1       1            1               u              u         u        u]
-co2max          = [3.1    4      3.2    1      1.5            1       1            1               14             1         1        1]
-namevarsco2     = ({'\delta Gross Production)','\delta Net Production','\delta Leaf Area Index','QVEGT','\delta Total Veg Carbon' ,'BTRANMN' ,'COST NFIX','COST RETRANS' ,'\delta N uptake cost','NACTIVE','NFIX','NRETRANS'})
+vars         = ({'GPP','NPP','TLAI','QVEGT','TOTVEGC','BTRANMN','COST_NFIX','COST_NRETRANS','NPP_NUPTAKE','NACTIVE','NFIX','NRETRANS','LEAFN'})
+namevars     = ({'Gross Production (gCm^{-2}y^{-1})','Net Production (gCm^{-2}y^{-1})','Leaf Area Index (m^{2}m^{-2})','QVEGT','Total Veg Carbon (gm^{-2})' ,'BTRANMN' ,'COST NFIX','COST RETRANS' ,'N uptake cost (gCm^{-2}y^{-1})','NACTIVE','NFIX','NRETRANS','LEAFN'})
+miny            = [3550  1400    6    1500    15700             1       10           10           0.4              10         10         5    0]
+maxy            = [3350  1100    7    1500    19000             1       10           10              0.4           10         10       5      3]
+unit_conversion = [u      u      1      u      1             1       1            1               u              u         u        u  1]
+co2max          = [3.1    4      3.2    1      1.5            1       1            1               14             1         1        1 1]
+co2max          = co2max.*0 +1.9
+namevarsco2     = ({'\delta Gross Production)','\delta Net Production','\delta Leaf Area Index','QVEGT','\delta Total Veg Carbon' ,'BTRANMN' ,'COST NFIX','COST RETRANS' ,'\delta N uptake cost','NACTIVE','NFIX','NRETRANS','\delta LEAFN'})
 
 params  = {'slatop','froot:leaf','stem:leaf','ncosts',...
 'fracfixers','leafcn','grperc','medlynslope',...
 'lmr-intercept','frac-ectomy-fungi','cn-flex-a','cn-flex-b',...
 'cn-flex-c','luna'}
 
-root = 'FUN_sens_ens_C_PI'
-root = 'FUN_sens_ens_transient_PI'
-root = 'FUN_sens_ens_D_PI'
-root = 'FUN_sens_ens_transient_D_PI'
+
 root = 'CLM5_1x1pt_Br-cax_ens_PI'
 ending = '_ens_transient_ELEV_'
 sitenames = {'1x1pt_Br-cax' 'bci_0.1x0.1_v4.0i' '1x1pt_US-ORN'}
 
 ichoose = 1:4  % loop roun iterations for each parameter
-vchoose = [1 2 9 3 5]; % variables.
-ychoose = 1852; %choose year
+vchoose = [1 2 3 5 13]; % variables.
+ychoose = 1995; %choose year
 mchoose = 6 %6:9 %choose month
 fileend = '.nc'
  pr=1 %are we printing figures?
@@ -47,20 +45,41 @@ fleafcn=0
 fmrf=0
 arc=1 %in archive? 
 nploth = 2;nplotv=1 %plots
-co2response =0
+co2response =1
 
 imap = [1 2 4 5]
+N=13
+
+X = linspace(0,pi*3,1000); 
+Y = bsxfun(@(x,n)sin(x+2*n*pi/N), X.', 1:N); 
+colord = linspecer(N); 
+colord = [1.0  0.0  0.0;...
+          0.7  0.0  0.0;...
+          0.8  0.1  0.4;...
+          0.8  0.6  0.1;...
+          0.0  0.0  1.0;...
+          0.0  0.6  0.8;...
+          0.5  0.0  0.8;...
+          0.4  0.4  0.4;...
+          0.1  0.1  0.1;...
+          0.8  0.8  0.8;...
+          0.0  1.0  0.0;...
+          0.1  0.8  0.6;...
+          0.6  0.8  0.1]
+
+
+set(0,'DefaultAxesColorOrder',colord)
 
 for s=schoose
   root = strcat('CLM5_',char(sitenames(s)),ending,'PI')
-
+  clear('var_arrayl','var_array2');
   for pp =[5]
 
     if(pp==1);pchoose=[ 1 2 3];end
     if(pp==2);pchoose=[ 4 5 6 7];end
     if(pp==3);pchoose=[ 11 12 13 ];end
     if(pp==4);pchoose=[ 8 9 14];end 
-    if(pp==5);pchoose=[ 1:14];end   
+    if(pp==5);pchoose=[ 1:13];end   
 
 
     for p=pchoose %parameter loop. 
@@ -88,25 +107,23 @@ for s=schoose
 
 	      var_arrayl(p,imap(i),v) = sum(vm)/length(mchoose);
              if(v==9);var_arrayl(p,imap(i),v) = var_arrayl(p,imap(i),v)./var_arrayl(p,imap(i),2);end
+            if(v==13);var_arrayl(p,imap(i),v) = var_arrayl(p,imap(i),v)./var_arrayl(p,imap(i),3);end
 	   end % v
 	end	%i
     end %p
 
 
     %read default
-    dirname = 'FACE_ORNL_BASELINE_TRANSIENT_clm4_5_16_r260_fates_amazon'
-    dirname = 'FUN_sens_ens_D_PI00'
-    dirname = 'FUN_sens_ens_transient_D_PI00'
 
-    dirname = strcat(root,'00')
+    defdirname = strcat(root,'00')
 
 
-    dir_clm = strcat('/glade/scratch/rfisher/archive/',dirname,'/lnd/hist/')
-    dir_clm = strcat('/glade/scratch/rfisher/',dirname,'/run/')
+    dir_clm = strcat('/glade/scratch/rfisher/archive/',defdirname,'/lnd/hist/')
+    dir_clm = strcat('/glade/scratch/rfisher/',defdirname,'/run/')
     for v = vchoose			
       for y = ychoose
 	 for m=mchoose								
-	    filen = strcat(dirname,'.clm2.h0.',num2str(y,'%04d'),'-',num2str(m,'%02d'),'.nc');
+	    filen = strcat(defdirname,'.clm2.h0.',num2str(y,'%04d'),'-',num2str(m,'%02d'),'.nc');
 	    filename = strcat(dir_clm,filen)
 	    rawvar = ncread(filename,char(vars(v)));
 	    vm(m) = rawvar(1).*unit_conversion(v);						     
@@ -116,6 +133,7 @@ for s=schoose
       for p=pchoose
         var_arrayl(p,i,v) = sum(vm)/length(mchoose);
 if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
+if(v==13);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),3);end
       end
     end % v
 
@@ -129,11 +147,10 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
        vrf=vr %/vr(2,1);
        h=plot(vrf,'.-','markersize',16);axis tight
        ylabel(namevars(v));
-       %xlim([0.5 6.5])
-       %ylim([ 0.5 1.5])
        % ylim([ min(min(var_array(:,:,v)))*0.5 max(max(var_array(:,:,v)))*1.5])
        vcount = vcount+1
        ylim([0 maxy(v)])
+       axis tight
        set(gca,'XTickLabels',[-1 0 1],'XTick',[1 3 5])
     end
 
@@ -151,7 +168,7 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
     
     
     if(co2response==1)
-    ychoose = 2004
+    ychooseco2 = 2004
        for p=pchoose %parameter loop. 
 	   pstr=char(num2str(p))
 	   for i=ichoose %iteration loop. 
@@ -166,7 +183,7 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
 	      end
 
 	      for v = vchoose			
-		 for y = ychoose 
+		 for y = ychooseco2 
 		    for m=mchoose								
 		       filen = strcat(dirname,'.clm2.h0.',num2str(y,'%02d'),'-',num2str(m,'%02d'),'.nc');
 		       filename = strcat(dir_clm,filen)
@@ -177,18 +194,21 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
 
 		 var_array2(p,imap(i),v) = sum(vm)/length(mchoose);
                   if(v==9);var_array2(p,imap(i),v) = var_array2(p,imap(i),v)./var_array2(p,imap(i),2);end
+                  if(v==13);var_array2(p,imap(i),v) = var_array2(p,imap(i),v)./var_array2(p,imap(i),3);end
 	      end % v
 	   end	%i
        end %p
        
        %read default
-       dirname = 'FUN_sens_ens_transient_D_PI00'
 
-       dir_clm = strcat('/glade/scratch/rfisher/',dirname,'/run/')
+
+       dir_clm = strcat('/glade/scratch/rfisher/',defdirname,'/run/')
+% dir_clm = strcat('/glade/scratch/rfisher/archive/',defdirname,'/lnd/hist/')
+
        for v = vchoose			
-	 for y = ychoose 
+	 for y = ychooseco2 
 	    for m=mchoose								
-	       filen = strcat(dirname,'.clm2.h0.',num2str(y,'%02d'),'-',num2str(m,'%02d'),'.nc');
+	       filen = strcat(defdirname,'.clm2.h0.',num2str(y,'%02d'),'-',num2str(m,'%02d'),'.nc');
 	       filename = strcat(dir_clm,filen)
 	       rawvar = ncread(filename,char(vars(v)));
 	       vm(m) = rawvar(1).*unit_conversion(v);						     
@@ -197,7 +217,8 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
 	 i=3
 	 for p=pchoose
            var_array2(p,i,v) = sum(vm)/length(mchoose);
-          if(v==9);var_array2(p,(i),v) = var_array2(p,(i),v)./var_array2(p,(i),2);end
+          if(v==9); var_array2(p,(i),v) = var_array2(p,(i),v)./var_array2(p,(i),2);end
+          if(v==13);var_array2(p,(i),v) = var_array2(p,(i),v)./var_array2(p,(i),3);end
 	 end
        end % v
 
@@ -218,7 +239,8 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
 	  h=plot(vrf,'.-','markersize',16);axis tight
 	  ylabel(namevarsco2(v));
 	  %xlim([0.5 6.5])
-	  ylim([ 1 co2max(v)])
+%	  ylim([ 1 co2max(v)])
+          axis tight
 	  % ylim([ min(min(var_array(:,:,v)))*0.5 max(max(var_array(:,:,v)))*1.5])
 	  vcount = vcount+1
 	  %ylim([0 maxy(v)])
@@ -233,7 +255,7 @@ if(v==9);var_arrayl(p,(i),v) = var_arrayl(p,(i),v)./var_arrayl(p,(i),2);end
        set(l,'position', [0.7551    0.2585    0.1393    0.0729])
        if(pr==1)
 	 wysiwyg
-	 fnm = strcat(figdir,'/frac_deviation_CO2_npp_',char(num2str(pchoose(1))),root,'_y',char(num2str((pchoose(1)))),'.png') 
+	 fnm = strcat(figdir,'/frac_deviation_CO2_response_',char(num2str(pchoose(1))),root,'_y',char(num2str((pchoose(1)))),'.png') 
 	 print('-dpng', '-r100', char(fnm) )
        end
        
